e8de2dc6c954d347210f6a227b9f0c0d2bc6afac
NinV <quannl71290@gmail.com>
2020-05-22 16:14:17 +0900

    remove classname

--- evaluate.py
+++ evaluate.py
@@ -3,24 +3,18 @@ import argparse
 from metrics import Evaluator
 from model import LPRNet
 
-classnames = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-              "가", "나", "다", "라", "마", "거", "너", "더", "러",
-              "머", "버", "서", "어", "저", "고", "노", "도", "로",
-              "모", "보", "소", "오", "조", "구", "누", "두", "루",
-              "무", "부", "수", "우", "주", "허", "하", "호"
-              ]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--label", required=True, help="Path to label file (training set)")
     parser.add_argument("-i", "--img_dir", required=True, help="Path to image folder (training set)")
     parser.add_argument("-w", "--weights", required=True, help="path to weights file")
-    parser.add_argument("-b", "--batch_size", type=int, default=32)
+    parser.add_argument("-b", "--batch_size", type=int, default=1000)
     args = vars(parser.parse_args())
 
+    evaluator = Evaluator(None, args["label"], args["img_dir"], args["batch_size"])
     tf.compat.v1.enable_eager_execution()
-    net = LPRNet(len(classnames) + 1)
+    net = LPRNet(len(evaluator.loader.class_names) + 1)
     net.load_weights(args["weights"])
-
-    evaluator = Evaluator(net, args["label"], args["img_dir"], args["batch_size"])
+    evaluator.model = net
     evaluator.evaluate()
